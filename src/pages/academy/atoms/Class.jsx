bcbb0eb6067d5629c92d4a9a1abc30e4d7a5b1e6
최지은 <jieun@choejieun-ui-MacBookAir.local>
2022-10-15 01:16:58 +0900

    2022-10-15 design: publishing

--- src/pages/academy/atoms/Class.jsx
+++ src/pages/academy/atoms/Class.jsx
@@ -10,7 +10,7 @@ const Class = () => {
           사진
         </Image>
         <TextBox>
-          <Tag>태그</Tag>
+          <Tag>취미기초반</Tag>
           <h4>나도 할 수 있을까? 너도 할 수 있어!</h4>
           <p>춤을 처음 배우시는 분, 새로운 취미, 문화생활을 원하시는 분, 일상의 스트레스 해소 혹은 다이어트 등이 필요하신 분!<br />재밌고, 즐겁게! 남녀노소 누구나 다햠께 춤을 즐길 수 있는 클래스</p>
         </TextBox>
@@ -26,24 +26,14 @@ const Wrap = styled.div`
   display: flex;
   flex-direction: column;
 
-  h1 {
-    font-family: 'Gmarket Sans TTF';
-    font-style: normal;
-    font-weight: 700;
-    font-size: 76px;
-    line-height: 100px;
-    color: #FFFFFF;
-    margin: 0px 0px 70px 100px;
-  }
-
-  p {
+  h2 {
     font-family: 'Pretendard';
     font-style: normal;
-    font-weight: 500;
-    font-size: 16px;
-    line-height: 28px;
-    color: #FFFFFF;
-    margin: 0px 0px 35px 100px;
+    font-weight: 400;
+    font-size: 36px;
+    line-height: 54px;
+    color: #000000;
+    margin: 120px 0px 80px 100px;
   }
 `
 
@@ -54,6 +44,7 @@ const ClassList = styled.div`
   flex-direction: row;
   align-items: center;
   background-color: yellow;
+  margin: 0px 0px 25px 100px;
 
   h4 {
     font-family: 'Pretendard';
@@ -80,12 +71,21 @@ const Image = styled.div`
   width: 300px;
   height: 180px;
   background-color: #A6D8B4;
+  margin-right: 36px;
 `
 
 const Tag = styled.div`
   width: 130px;
   height: 40px;
   background-color: #000000;
+  font-family: 'Pretendard';
+  font-style: normal;
+  font-weight: 500;
+  font-size: 16px;
+  color: #FFFFFF;
+  display: flex;
+  justify-content: center;
+  align-items: center;
 `
 
 const TextBox = styled.div`
