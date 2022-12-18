f58dbe711f3b97bcbfd7619e4f39685cc68083e0
JIEUN <pandaa0628@gmail.com>
2022-12-18 18:01:40 +0900

    schedule

--- src/pages/academy/atoms/Schedule.jsx
+++ src/pages/academy/atoms/Schedule.jsx
@@ -1,10 +1,268 @@
-import React from "react";
-import styled from "styled-components";
+import React from 'react';
+import styled from 'styled-components';
+import { BsCircleFill } from 'react-icons/bs';
+import { AiOutlineLine } from 'react-icons/ai';
 
 const Schedule = () => {
   return (
     <Wrap>
       <h1>JULY SCHEDULE</h1>
+      <Contents>
+        <Time>
+          <h2>MON</h2>
+          <TimeBox>
+            <TimeContents>
+              <hr />
+              <span>18:00 - 19:00</span>
+            </TimeContents>
+            <TimeContents>
+              <hr />
+              <span>19:00 - 20:00</span>
+            </TimeContents>
+            <TimeContents>
+              <hr />
+              <span>20:00 - 21:00</span>
+            </TimeContents>
+            <TimeContents>
+              <hr />
+              <span>21:00 - 22:00</span>
+            </TimeContents>
+          </TimeBox>
+        </Time>
+        <DayBox>
+          <h2>MON</h2>
+          <Day>
+            <DayContents>
+              <div>
+                <BsCircleFill size={10} color='#79EC43' />
+                <span>브레이킹</span>
+                <span className='teacher'>DAILL</span>
+              </div>
+            </DayContents>
+            <DayContents>
+              <div>
+                <BsCircleFill size={10} color='#FF6B6B' />
+                <span>걸리쉬</span>
+                <span className='teacher'>GAJIN</span>
+              </div>
+              <div>
+                <BsCircleFill size={10} color='#8ED5FD' />
+                <span>팝핑</span>
+                <span className='teacher'>MR.Z</span>
+              </div>
+            </DayContents>
+            <DayContents>
+              <div>
+                <BsCircleFill size={10} color='#FF6B6B' />
+                <span>케이팝</span>
+                <span className='teacher'>TAENI</span>
+              </div>
+              <div>
+                <BsCircleFill size={10} color='#FF6B6B' />
+                <span>브레이킹</span>
+                <span className='teacher'>BINZ</span>
+              </div>
+            </DayContents>
+            <DayContents>
+              <div>
+                <BsCircleFill size={10} color='#BD70FA' />
+                <span>브레이킹</span>
+                <span className='teacher'>BAM</span>
+              </div>
+            </DayContents>
+          </Day>
+        </DayBox>
+        <DayBox>
+          <h2>TUE</h2>
+          <Day>
+            <DayContents>
+              <div>
+                <BsCircleFill size={10} color='#79EC43' />
+                <span>브레이킹</span>
+                <span className='teacher'>DAILL</span>
+              </div>
+            </DayContents>
+            <DayContents>
+              <div>
+                <BsCircleFill size={10} color='#FF6B6B' />
+                <span>걸리쉬</span>
+                <span className='teacher'>GAJIN</span>
+              </div>
+              <div>
+                <BsCircleFill size={10} color='#8ED5FD' />
+                <span>팝핑</span>
+                <span className='teacher'>MR.Z</span>
+              </div>
+            </DayContents>
+            <DayContents>
+              <div>
+                <BsCircleFill size={10} color='#FF6B6B' />
+                <span>케이팝</span>
+                <span className='teacher'>TAENI</span>
+              </div>
+              <div>
+                <BsCircleFill size={10} color='#FF6B6B' />
+                <span>브레이킹</span>
+                <span className='teacher'>BINZ</span>
+              </div>
+            </DayContents>
+            <DayContents>
+              <div>
+                <BsCircleFill size={10} color='#BD70FA' />
+                <span>브레이킹</span>
+                <span className='teacher'>BAM</span>
+              </div>
+            </DayContents>
+          </Day>
+        </DayBox>
+        <DayBox>
+          <h2>WED</h2>
+          <Day>
+            <DayContents>
+              <div>
+                <BsCircleFill size={10} color='#79EC43' />
+                <span>브레이킹</span>
+                <span className='teacher'>DAILL</span>
+              </div>
+            </DayContents>
+            <DayContents>
+              <div>
+                <BsCircleFill size={10} color='#FF6B6B' />
+                <span>걸리쉬</span>
+                <span className='teacher'>GAJIN</span>
+              </div>
+              <div>
+                <BsCircleFill size={10} color='#8ED5FD' />
+                <span>팝핑</span>
+                <span className='teacher'>MR.Z</span>
+              </div>
+            </DayContents>
+            <DayContents>
+              <div>
+                <BsCircleFill size={10} color='#FF6B6B' />
+                <span>케이팝</span>
+                <span className='teacher'>TAENI</span>
+              </div>
+              <div>
+                <BsCircleFill size={10} color='#FF6B6B' />
+                <span>브레이킹</span>
+                <span className='teacher'>BINZ</span>
+              </div>
+            </DayContents>
+            <DayContents>
+              <div>
+                <BsCircleFill size={10} color='#BD70FA' />
+                <span>브레이킹</span>
+                <span className='teacher'>BAM</span>
+              </div>
+            </DayContents>
+          </Day>
+        </DayBox>
+        <DayBox>
+          <h2>THU</h2>
+          <Day>
+            <DayContents>
+              <div>
+                <BsCircleFill size={10} color='#79EC43' />
+                <span>브레이킹</span>
+                <span className='teacher'>DAILL</span>
+              </div>
+            </DayContents>
+            <DayContents>
+              <div>
+                <BsCircleFill size={10} color='#FF6B6B' />
+                <span>걸리쉬</span>
+                <span className='teacher'>GAJIN</span>
+              </div>
+              <div>
+                <BsCircleFill size={10} color='#8ED5FD' />
+                <span>팝핑</span>
+                <span className='teacher'>MR.Z</span>
+              </div>
+            </DayContents>
+            <DayContents>
+              <div>
+                <BsCircleFill size={10} color='#FF6B6B' />
+                <span>케이팝</span>
+                <span className='teacher'>TAENI</span>
+              </div>
+              <div>
+                <BsCircleFill size={10} color='#FF6B6B' />
+                <span>브레이킹</span>
+                <span className='teacher'>BINZ</span>
+              </div>
+            </DayContents>
+            <DayContents>
+              <div>
+                <BsCircleFill size={10} color='#BD70FA' />
+                <span>브레이킹</span>
+                <span className='teacher'>BAM</span>
+              </div>
+            </DayContents>
+          </Day>
+        </DayBox>
+        <DayBox>
+          <h2>FRI</h2>
+          <Day>
+            <DayContents>
+              <div>
+                <BsCircleFill size={10} color='#79EC43' />
+                <span>브레이킹</span>
+                <span className='teacher'>DAILL</span>
+              </div>
+            </DayContents>
+            <DayContents>
+              <div>
+                <BsCircleFill size={10} color='#FF6B6B' />
+                <span>걸리쉬</span>
+                <span className='teacher'>GAJIN</span>
+              </div>
+              <div>
+                <BsCircleFill size={10} color='#8ED5FD' />
+                <span>팝핑</span>
+                <span className='teacher'>MR.Z</span>
+              </div>
+            </DayContents>
+            <DayContents>
+              <div>
+                <BsCircleFill size={10} color='#FF6B6B' />
+                <span>케이팝</span>
+                <span className='teacher'>TAENI</span>
+              </div>
+              <div>
+                <BsCircleFill size={10} color='#FF6B6B' />
+                <span>브레이킹</span>
+                <span className='teacher'>BINZ</span>
+              </div>
+            </DayContents>
+            <DayContents>
+              <div>
+                <BsCircleFill size={10} color='#BD70FA' />
+                <span>브레이킹</span>
+                <span className='teacher'>BAM</span>
+              </div>
+            </DayContents>
+          </Day>
+        </DayBox>
+      </Contents>
+      <Footer>
+        <div>
+          <BsCircleFill size={10} color='#FF6B6B' />
+          <span>취미반</span>
+        </div>
+        <div>
+          <BsCircleFill size={10} color='#79EC43' />
+          <span>키즈반</span>
+        </div>
+        <div>
+          <BsCircleFill size={10} color='#8ED5FD' />
+          <span>직장인반</span>
+        </div>
+        <div>
+          <BsCircleFill size={10} color='#BD70FA' />
+          <span>전문반</span>
+        </div>
+      </Footer>
     </Wrap>
   );
 };
@@ -14,17 +272,103 @@ const Wrap = styled.div`
   height: 1528px;
   display: flex;
   flex-direction: column;
+  align-items: center;
   background-color: #000000;
+  font-family: 'Pretendard';
+  font-style: normal;
+  color: #ffffff;
 
   h1 {
-    font-family: "Pretendard";
-    font-style: normal;
+    width: 80%;
     font-weight: 700;
     font-size: 40px;
     line-height: 52px;
     color: #ffffff;
-    margin: 120px 0px 80px 100px;
+    margin: 120px 0px 80px 0px;
   }
 `;
 
+const Contents = styled.div`
+  width: 80%;
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+`;
+
+const Time = styled.div`
+  width: 160px;
+`;
+
+const TimeBox = styled.div`
+  width: 100%;
+  min-height: 540px;
+  display: flex;
+  flex-direction: column;
+`;
+
+const TimeContents = styled.div`
+  display: flex;
+  flex-direction: column;
+  height: 130px;
+
+  hr {
+    display: block;
+    width: 40%;
+    color: white;
+    margin: 0px 130px 22px 0px;
+  }
+`;
+
+const DayBox = styled.div`
+  width: 156px;
+  margin-left: 20px;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+`;
+
+const Day = styled.div`
+  width: 100%;
+  min-height: 540px;
+  padding: 10px 0px;
+  border: 2px solid #333333;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+`;
+
+const DayContents = styled.div`
+  width: 80%;
+  min-height: 130px;
+
+  span {
+    margin-left: 10px;
+    font-size: 14px;
+    font-weight: 700;
+  }
+
+  .teacher {
+    font-size: 12px;
+    color: #c1c1c1;
+  }
+`;
+
+const Footer = styled.div`
+  width: 80%;
+  height: 100px;
+  margin-top: 24px;
+  display: flex;
+  flex-direction: row;
+  justify-content: flex-end;
+
+  div {
+    margin-left: 28px;
+
+    span {
+      margin-left: 10px;
+    }
+  }
+`;
 export default Schedule;
